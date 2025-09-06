cda58d3578390e20db8eb97e0ea011cb0d07d45e
TaliaR <natalia.1067@hotmail.com>
2025-09-06 16:22:36 -0300

    add-mudancas

--- PooBasica/src/App.java
+++ PooBasica/src/App.java
@@ -1,11 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        ContaBancaria c1 = new ContaBancaria("12345678901", new int[]{1,2,3,4,5}, 100d, 50d);
-        // c1.darLimiteEspecial(250d);
-        // System.out.println(c1.sacar(30d));
-        // System.out.println(c1.saldoTotal());
-
-        System.out.println(c1.sacar(150d));
-        // System.out.println(c1.depositar(25d));
+        System.out.println("Hello World");
     }
 }
