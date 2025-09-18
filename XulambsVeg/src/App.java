b4c3f29d0acbd6aaceb6d054062b7319d7206bed
Talia-R <natalia.1067@hotmail.com>
2025-09-18 17:30:27 -0300

    finalizando primeira versão XulambsVeg

--- XulambsVeg/src/App.java
+++ XulambsVeg/src/App.java
@@ -1,35 +1,127 @@
 import java.text.NumberFormat;
-import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
-        int escolha;
+    static LinkedList<Pizza> pizzas = new LinkedList<>();
+    static NumberFormat moeda = NumberFormat.getCurrencyInstance();
 
-        System.out.println(cardapio());
+    public static String detalheDivisor(String divisor, int repeticoes){
+        return divisor.repeat(repeticoes);
+    }
 
-        escolha = Integer.parseInt(entrada.nextLine());
-        switch(escolha){
-            case 1:
-        }
+    public static String detalheDivisorTraco(){
+        return "-".repeat(20);
+    }
 
-        entrada.close();
+    public static String cabecalho(){
+        return " --- Bem-vindo(a) ao XulambsVeg! ---";
     }
 
     public static String cardapio(){
         StringBuilder s = new StringBuilder();
-        NumberFormat moeda = NumberFormat.getCurrencyInstance();
-        s.append(" --- Bem-vindo(a) ao XulambsVeg! ---");
-        s.append("\nCardápio: ");
-        s.append("\nPizza: " + moeda.format(Pizza.getPrecoPadrao()));
+        s.append("Cardápio: ");
+        s.append("\n0) Sair");
+        s.append("\n1) Pizza: " + moeda.format(Pizza.getPrecoPadrao()));
+        s.append("\n");
+        s.append(detalheDivisorTraco());
         
         return s.toString();
+    }
+
+    /**
+     * Cria um objeto pizza e o adiciona na lista de Pizzas.
+     * Exibe o relatorio (nota de compra).
+     */
+    public static void comprarPizza(){
+        Pizza novaPizza = new Pizza();
+        montarPizza(novaPizza);
+        pizzas.add(novaPizza);
+
+        System.out.println(relatorio());
+        System.out.println(detalheDivisorTraco());
+
+    }
+
+
+    public static void montarPizza(Pizza pizza){
+        System.out.print("Vamos montar sua pizza: ");
+
+        int qntAdicionais = InputUtils.lerInt("\nQuantos adicionais gostaria de incluir (máx: " + 
+                                                Pizza.getMaxIngredientesAdicionais() +
+                                                ")?: ");
+
+        pizza.editarPizza(1, qntAdicionais);
     }
 
     public static String relatorio(){
         StringBuilder s = new StringBuilder();
 
-        
+        for(int i = 0; i < pizzas.size(); i++){
+            s.append(String.format("%d) %s", (i+1), pizzas.get(i).relatorio()));
+
+            if(i >= 1){
+                int precoFinal = 0;
+                for(Pizza pizza: pizzas){
+                    precoFinal += pizza.getPrecoFinal();
+                }
+                s.append("\nTotal Pedido: " + moeda.format(precoFinal));
+            }
+
+            // quebra de linha apos pizzas:
+            if(pizzas.size() >= 1 && i == pizzas.size() - 1){
+                break;
+            }
+            s.append("\n");
+        }
+
         return s.toString();
     }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(cabecalho());
+
+        int escolha = -1;
+        do{
+        try{
+       
+            System.out.println(cardapio());
+            escolha = InputUtils.lerInt("Opção:  ");
+            switch(escolha){
+                case 1 -> comprarPizza();
+            //    case 3 -> editarPedido();
+                case 0 -> {
+                    System.out.print("Até a próxima! =^.^=");
+                    continue;
+                }
+            }
+            escolha = InputUtils.lerInt("Gostaria de pedir algo mais? 0) Não | 1) Sim ");
+            System.out.print("Até a próxima! =^.^=");
+            
+        } catch (NullPointerException npe){
+            System.out.print("Entrada vazia: " + npe.getMessage());
+        } catch (InputMismatchException ime){
+            System.out.println("Valor de entrada inválida: " + ime.getMessage());
+        } catch(NumberFormatException nfe){
+            System.out.println("Digite um número válido: " + nfe.getMessage());
+        }
+    } while (escolha != 0);
+
+        InputUtils.fechar();
+    }
+
+
+    /*
+     * pro futuro
+     * public static void editarPedido(){
+     *  int escolha = InputUtils.lerInt("1) Concluir Pedido | 2) Editar Pizza");
+
+        while (escolha == 2) {
+        escolha = InputUtils.lerInt("1) Adicionar Ingredientes | 2) Remover Ingredientes");
+        int novaQntIngredientes = InputUtils.lerInt("\nQuantos ingredientes?: ");
+        novaPizza.editarPizza(escolha, novaQntIngredientes);
+        }
+    }
+     */
+
 }
--- XulambsVeg/src/Pizza.java
+++ XulambsVeg/src/Pizza.java
@@ -1,7 +1,9 @@
+import java.text.NumberFormat;
+
 public class Pizza {
     private static final int MAX_INGREDIENTES_ADICIONAIS = 8;
     private static final int CUSTO_ADICIONAIS = 5;
-    private static int preco = 29;
+    private static int precoBase = 29;
     private int qntAdicionaisAtual = 0;
 
     public Pizza(){};
@@ -11,14 +13,18 @@ public class Pizza {
     }
 
     public static int getPrecoPadrao(){
-        return preco;
+        return precoBase;
     }
 
+    public static int getMaxIngredientesAdicionais(){
+        return MAX_INGREDIENTES_ADICIONAIS;
+    }
+    
     /**
      * Verifica se a pizza atingiu o máximo de ingredientes adicionais.
      * @return true caso tenho atingindo o maior de ingredientes adicionais
      */
-    public boolean atingiuMaxAdicionais(){
+    private boolean atingiuMaxAdicionais(){
         return qntAdicionaisAtual > MAX_INGREDIENTES_ADICIONAIS;
     }
 
@@ -26,11 +32,76 @@ public class Pizza {
      * Adiciona ingredientes adicionais a pizza caso ela não tenha ultrapassado o valor máximo de ingredientes adicionais. 
      * @param qntAdicionais inteiro com a quantidade de adicionais a serem adicionadas
      */
-    public void colocarAdicionais(int qntAdicionais){
-        if(atingiuMaxAdicionais()){
-            throw new IllegalArgumentException("Atingiu o máximo de ingredientes");
-        }
+    private void incluirAdicionais(int qntAdicionais){
         qntAdicionaisAtual += qntAdicionais;
+        if(atingiuMaxAdicionais()){
+            qntAdicionaisAtual = MAX_INGREDIENTES_ADICIONAIS;
+           // throw new IllegalArgumentException("Atingiu o máximo de ingredientes");
+        }
+    }
+
+    /**
+     * Remove ingredientes adicionais a pizza caso ela não tenha ultrapassado o valor máximo de ingredientes adicionais. 
+     * @param qntAdicionais inteiro com a quantidade de adicionais a serem removidas
+     */
+    private void removerAdicionais(int qntAdicionais){
+        qntAdicionaisAtual -= qntAdicionais;
+        if(qntAdicionaisAtual < 0){
+            qntAdicionaisAtual = 0;
+           // throw new IllegalArgumentException("Atingiu o máximo de ingredientes");
+        }
+    }
+
+    /**
+     * Edita uma pizza podendo incluir ou remover ingredientes
+     * @param escolha int que decide qual operação quer fazer (adicionar ou remover) ingredientes
+     * @param novaQntIngredientes int com a quantidade de ingredientes que quer editar
+     */
+    public void editarPizza(int escolha, int novaQntIngredientes){
+        if(escolha == 1){
+            incluirAdicionais(novaQntIngredientes);
+        } else if(escolha == 2){
+            removerAdicionais(novaQntIngredientes);
+        }
+    }
+
+    /**
+     * Calcula o valor dos adicionais da pizza.
+     * @return double com a soma dos valores adicionais
+     */
+    private double calcularPrecoAdicional(){
+        return qntAdicionaisAtual * CUSTO_ADICIONAIS;
+    }
+
+    /**
+     * Calcula o valor final da pizza. Considera o valor padrão e os valores dos adicionais
+     * @return double com a soma do valor padrão e adicionais
+     */
+    private double precoFinal(){
+        return precoBase + calcularPrecoAdicional();
+    }
+
+    public double getPrecoFinal(){
+        return precoFinal();
+    }
+
+    /**
+     * Exibe um relatório da pizza com descrição dos ingredientes (caso tenha) e seu preço.
+     * @return string com a descrição e preço da pizza
+     */
+    public String relatorio(){
+        NumberFormat moeda = NumberFormat.getCurrencyInstance();
+        StringBuilder s = new StringBuilder();
+        s.append("Pizza no valor de: " + moeda.format(getPrecoPadrao()));
+        if(qntAdicionaisAtual > 0){
+            s.append(String.format(
+                " com %d adicionais no valor de %s", 
+                qntAdicionaisAtual, 
+                moeda.format(calcularPrecoAdicional())
+                ));
+            s.append(" | Total: " + moeda.format(precoFinal()));
+        }
+            return s.toString();
     }
 
     //a pizza vai saber oq vendeu a partir do relatorio
