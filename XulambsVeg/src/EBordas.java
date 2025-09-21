d6b1c8c045fe413f3dfcdd9cac7141851a19aa49
Talia-R <natalia.1067@hotmail.com>
2025-09-21 13:32:09 -0300

    inicializando Enum EBordas

---
+++ XulambsVeg/src/EBordas.java
@@ -0,0 +1,25 @@
+import java.text.NumberFormat;
+
+public enum EBordas {
+    SEM_BORDA("Borda Tradiciona", 0),
+    REQUEIJAO("Borda de Requeijão", 7d),
+    CHEDDAR("Borda de Cheddar", 10d),
+    CHOCOLATE("Borda de Chocolate", 8d);
+
+    private String descricao;
+    private double preco;
+
+    private EBordas(String descricao, double preco){
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public String getDescricao(){
+        NumberFormat moeda = NumberFormat.getCurrencyInstance();
+        return String.format("%s: %s", descricao, moeda.format(preco));
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+}
