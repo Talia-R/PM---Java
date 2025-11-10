32c613889cd3e3951b7a4d17a82766858f85d952
Talia-R <natalia.1067@hotmail.com>
2025-11-10 08:22:02 -0300

    init front

--- XulambsVeg/src/models/IComida.java
+++ XulambsVeg/src/models/IComida.java
@@ -2,6 +2,4 @@ package models;
 
 public interface IComida {
     public double precoFinal();
-    public void editarQntIngredPizza(int escolha, int novaQntIngredientes);
-    public double calcularPrecoAdicional();
 }
---
+++ XulambsVeg/src/models/Sanduiche.java
@@ -0,0 +1,5 @@
+// package models;
+
+// public class Sanduiche extends Comida {
+    
+// }
